d18e0c75d7216687b0e610146f1f36c416760b57
Bagas <chayyan.bk@gmail.com>
2022-04-28 17:24:15 +0800

    Home

---
+++ app/Views/About.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Web | About</title>
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <nav>
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Contact</a></li>   
+                </ul>
+            </nav>
+        </div>
+        <main class="content">
+              <section class="hero">
+                  <img src="bagas2.JPG" alt="">
+              <div class="hero-content">
+                  <h1>ABOUT</h1><br></h2>Muhammad Chayyan Bagaskara</h2><br><br>   
+<p> Saya adalah karyawan Kontrak dari perusahaan PT. Indonesia Comnets Plus, salah satu dari anak perusahaan PT PLN Persero.Di perusahaan saya menjabat sebagai Cash Management pada Regional Bali dan Nusa Tenggara. 
+Demi meningkatkan skill saya melanjutkan belajar di Universitas Pendidikan Nasional,di Undiknas saya mengambil jurusan Teknologi Informasi Fakultas Teknik dan Informatika.Saya yakin perjuangan saya akan membuahkan hasil di kemudian hari.
+</p>
+              </div>
+                </section>
+        </div>
+        <div class="footer">
+            <footer>
+            <ul>
+                <li><img class="img" src="instagram.png" alt=""><p>Instagram</p></a></li>
+                <li><img class="img" src="facebook.png" alt=""><p>Facebook</p></a></li>
+            </ul>
+            </footer>
+        </div>
+    </div>    
+</body>
+</html>
--- app/Views/Home.php
+++ app/Views/Home.php
@@ -2,47 +2,40 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portofolio</title>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Web | Bagaskara</title>
 </head>
 <body>
-    <div class="wrapper">
-        
-        <div class="body">
-            <div class="header">
-                <img src="img/Logo Undiknas - CL.png" alt="" class="mg1">
-                <h1>UNIVERSITAS PENDIDIKAN NASIONAL</h1>
-            </div>
-            <div class="konten">
-                <img src="img/bagas2.JPG" alt="" class="mg2">
-                <h2 class="">Muhammad Chayyan Bagaskara </h2> 
-                <p>
-                    Saya adalah karyawan Kontrak dari perusahaan PT. Indonesia Comnets Plus, salah satu dari anak perusahaan PT PLN Persero. Di perusahaan saya menjabat sebagai Cash Management 
-                    pada Regional Bali dan Nusa Tenggara. Demi meningkatkan skill saya melanjutkan belajar di Universitas Pendidikan Nasional, di Undiknas saya mengambil jurusan Teknologi Informasi Fakultas Teknik dan Informatika. 
-                    Saya yakin perjuangan saya akan membuahkan hasil di kemudian hari.
-
-                </p>
-            
-            <br>
-            <P>Profile Saya dapat di lihat lebih lengkap di page 
-                <a href="profile.html" class="">selanjutnya</a>
-            </P>
-            </div>
-            <div class="footer"></div>
-        </div>
+    <div class="container">
         <div class="sidebar">
-            <p>
-                <h2>MENU</h2>
-            </p>
-            <h3>Portofolio</h3>
-            <ul>
-                <li><a href="home.html">HOME</a></li>
-                <li><a href="profile.html">Muhammad Chayyan Bagaskara</a></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><a href="About">About</a></li>
+                    <li><a href="Home">Home</a></li>
+                    <li><a href="Contact">Contact</a></li>   
+                </ul>
+            </nav>
         </div>
-    </div>
-    
+        <main class="content">
+              <section class="hero">
+                  <img src="bagas2.JPG" alt="">
+              <div class="hero-content">
+                  <h1>Walcome to My Website</h1><br></h2>Muhammad Chayyan Bagaskara</h2><br><br>   
+<p> Saya adalah karyawan Kontrak dari perusahaan PT. Indonesia Comnets Plus, salah satu dari anak perusahaan PT PLN Persero.Di perusahaan saya menjabat sebagai Cash Management pada Regional Bali dan Nusa Tenggara. 
+Demi meningkatkan skill saya melanjutkan belajar di Universitas Pendidikan Nasional,di Undiknas saya mengambil jurusan Teknologi Informasi Fakultas Teknik dan Informatika.Saya yakin perjuangan saya akan membuahkan hasil di kemudian hari.
+</p>
+              </div>
+                </section>
+        </div>
+        <div class="footer">
+            <footer>
+            <ul>
+                <li><img class="img" src="instagram.png" alt=""><p>Instagram</p></a></li>
+                <li><img class="img" src="facebook.png" alt=""><p>Facebook</p></a></li>
+            </ul>
+            </footer>
+        </div>
+    </div>    
 </body>
 </html>
